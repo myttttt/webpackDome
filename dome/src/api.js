b6b641ebad8187ef09d7722aab02bd2e41fd99ad
myttttt <2546922538@qq.com>
2019-03-28 19:39:37 +0800

    已修改

--- dome/src/api.js
+++
@@ -1,8 +0,0 @@
-
-let a=1;
-let b=2;
-
-module.exports=function(){
-	console.log(a+b)
-	return a+b;
-}
--- dome/src/index.js
+++
@@ -1,7 +0,0 @@
-console.log('webpack')
-
-let api=require('./api.js')
-
-window.onload=function(){
-	api();
-}
